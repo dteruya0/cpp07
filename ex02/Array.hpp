63a4a04febd6242b88880deda0e070f2f6c92cb3
Teruya <diegokteruya@gmail.com>
2026-02-04 20:59:52 -0300

    new uploads

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: dteruya <dteruya@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 19:45:14 by dteruya           #+#    #+#             */
-/*   Updated: 2026/02/03 20:37:56 by dteruya          ###   ########.fr       */
+/*   Updated: 2026/02/04 19:00:16 by dteruya          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define ARRAY_HPP
 
 #include <iostream>
+#include <stdexcept>
 #define DEBUG false
 
 template <typename T>
@@ -28,6 +29,9 @@ class Array
 		Array(unsigned int n);
 		Array(const Array& other);
 		Array& operator=(const Array& other);
+		T& operator[](unsigned int index);
+		const T& operator[](unsigned int index) const;
+		unsigned int size() const;
 		~Array();
 };
 
@@ -68,5 +72,30 @@ template <typename T>
 Array<T>::~Array() {
 	delete[] _data;
 }
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array& other)
+{
+	if (this != other)
+	{
+		delete[] data;
+
+		_size = other._size;
+		_data = new T[_size];
+		for (unsigned int i = 0; i < _size; i++)
+			_data[i] = other._data[i];
+	}
+	return *this;
+}
+
+template <typename T>
+T& Array<T>::operator[](unsigned int index)
+{
+	if (index >= _size)
+		throw std::out_of_range("Index out of bounds");
+	return _data[index];
+}
+
+template <typename T>
 
 #endif
